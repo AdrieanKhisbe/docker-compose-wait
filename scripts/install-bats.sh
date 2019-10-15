13b7201a6b003e0a896419893140b4e0883bd06c
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-10-15 21:21:17 +0200

    Rework multistage build

--- scripts/install-bats.sh
+++ scripts/install-bats.sh
@@ -4,7 +4,8 @@ set -e
 cd /tmp
 git clone --branch v0.4.0 https://github.com/sstephenson/bats.git
 cd bats
-./install.sh /usr/local
+sudo rm -rf /usr/local/bin/bats
+(sudo ./install.sh /usr/local)
 cd ..
 rm -rf bats
-bats
+bats -h
